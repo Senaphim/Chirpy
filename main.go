5a6f0a8ecf936fa6b1dc27895a68608a78ce084d
senaphim <senaphim@gmail.com>
2025-07-14 19:50:09 +0100

    restructed format for filserver and added health handler

--- main.go
+++ main.go
@@ -7,12 +7,23 @@ import (
 
 func main() {
 	serveMux := http.NewServeMux()
+	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	hh := http.HandlerFunc(handleHealth)
+	serveMux.Handle("/healthz", hh)
+
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
 	err := server.ListenAndServe()
 	if err != nil {
-		fmt.Println(fmt.Errorf("Error serving request:\n%v"))
+		fmt.Println(fmt.Errorf("Error serving request:\n%v", err))
 	}
+
+}
+
+func handleHealth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
